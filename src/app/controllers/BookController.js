baaf926b82421725ae7634eedbd21895dbd20ad4
Abner Borgonha <borgonha.abner.ruth@outlook.com>
2020-09-05 23:22:15 -0300

    Add method to list all books and list by title

--- src/app/controllers/BookController.js
+++ src/app/controllers/BookController.js
@@ -5,7 +5,8 @@ class BookController {
         const { title } = request.body;
 
         if (!title) {
-            return response.status(400).json({ error: 'Title not found, is mandatory.'});
+            return response.status(400)
+                .json({ error: 'Title not found, is mandatory.' });
         }
 
         const titleExit = await Book.findOne({ where: { title } });
@@ -17,6 +18,18 @@ class BookController {
         const book = await Book.create(request.body);
 
         return response.json({ book });
+    }
+
+    async index(request, response) {
+        const { title } = request.query;
+
+        const books = title? await Book.findOne({where: {title}}) : await Book.findAll();
+
+        if (!books) {
+            return response.status(400).json({ error: 'Book not found.' });
+        }
+
+        return response.json(books);
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import BookController from './app/controllers/BookController';
 
 const routes = new Router();
 
-routes.post('/book', BookController.store);
+routes.get('/books', BookController.index);
+routes.post('/books', BookController.store);
 
 export default routes;
