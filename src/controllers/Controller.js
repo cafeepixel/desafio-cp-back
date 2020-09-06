aaebe155ab03793c652652aad5c60916767a56c3
rogerioisj <rogerio.reaper@gmail.com>
2020-09-06 15:07:52 -0300

    Delete a resource is avaible now

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -11,6 +11,7 @@ exports.details = (req, res) => {
 exports.add = (req, res, next) => {
     Books.create(req.body).then((books) => {
         res.send(books);
+        console.log('ID: ' + req.params.id + " inserted on Database")
     }).catch(next);
 };
 
@@ -19,13 +20,14 @@ exports.update = (req, res, next) => {
     .then(() => {
         Books.findOne({_id: req.params.id}).then((books) => {
             res.send(books);
-            console.log('ID: ' + req.params.id + " Atualizado no BD")
+            console.log('ID: ' + req.params.id + " updated on Database")
         });
-    }).catch(next, console.log(next));
+    }).catch(next);
 };
 
 exports.delete = (req, res, next) => {
     Books.findByIdAndRemove({_id:req.params.id}).then((books) => {
         res.send(books);
+        console.log('ID: ' + req.params.id + " removed from Database")
     }).catch(next);
 };
